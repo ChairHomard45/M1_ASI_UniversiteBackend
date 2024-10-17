4ee579fac515bc60528f0f1517958b6224b95f92
ChairHomard45 <delepaut.clement@gmail.com>
2024-10-17 12:19:40 +0200

    Ninth Commit: Chapitre 3 - 8.1.1 (Debut)

---
+++ UniversiteDomain/UseCases/ParcoursUseCases/Create/CreateParcoursUseCase.cs
@@ -0,0 +1,27 @@
+using UniversiteDomain.DataAdapters;
+using UniversiteDomain.Entities;
+
+namespace UniversiteDomain.UseCases.ParcoursUseCases.Create;
+
+public class CreateParcoursUseCase(iParcoursRepository parcoursRepository)
+{
+    public async Task<Parcours> ExecuteAsync(string nomPar, int AnneeFor)
+    {
+        var parcours = new Parcours{NomParcours = nomPar, AnneeFormation = AnneeFor};
+        return await ExecuteAsync(parcours);
+    }
+    public async Task<Parcours> ExecuteAsync(Parcours parcours)
+    {
+        await CheckBusinessRules(parcours);
+        Parcours pa = await parcoursRepository.CreateAsync(parcours);
+        parcoursRepository.SaveChangesAsync().Wait();
+        return pa;
+    }
+    private async Task CheckBusinessRules(Parcours parcours)
+    {
+        ArgumentNullException.ThrowIfNull(parcours);
+        ArgumentNullException.ThrowIfNull(parcours.NomParcours);
+        ArgumentNullException.ThrowIfNull(parcours.AnneeFormation);
+        ArgumentNullException.ThrowIfNull(parcoursRepository);
+    }
+}
