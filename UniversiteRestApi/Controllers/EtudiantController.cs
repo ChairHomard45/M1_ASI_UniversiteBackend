b1163235f9580250192073d73511015d6b0dad73
ChairHomard45 <delepaut.clement@gmail.com>
2024-11-21 21:38:38 +0100

    Twenty Second Commit: Chapitre 6 - 1 (Début)

--- UniversiteRestApi/Controllers/EtudiantController.cs
+++ UniversiteRestApi/Controllers/EtudiantController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UniversiteDomain.DataAdapters.DataAdaptersFactory;
+using UniversiteDomain.Dtos;
 using UniversiteDomain.Entities;
 using UniversiteDomain.UseCases.EtudiantUseCases.Create;
 
@@ -25,12 +26,26 @@ namespace UniversiteRestApi.Controllers
         }
 
         // Crée un nouvel étudiant sans parcours
-        // POST api/<EtudiantApi>
+        // POST api/<EtudiantController>
         [HttpPost]
-        public async Task PostAsync([FromBody] Etudiant etudiant)
+        public async Task<ActionResult<EtudiantDto>> PostAsync([FromBody] EtudiantDto etudiantDto)
         {
-            CreateEtudiantUseCase uc=new CreateEtudiantUseCase(repositoryFactory.EtudiantRepository());
-            await uc.ExecuteAsync(etudiant);
+            CreateEtudiantUseCase createEtudiantUc = new CreateEtudiantUseCase(repositoryFactory.EtudiantRepository());           
+            Etudiant etud = etudiantDto.ToEntity();
+            try
+            {
+                etud = await createEtudiantUc.ExecuteAsync(etud);
+            }
+            catch (Exception e)
+            {
+                // On récupère ici les exceptions personnalisées définies dans la couche domain
+                // Et on les envoie avec le code d'erreur 400 et l'intitulé "erreurs de validation"
+                ModelState.AddModelError(nameof(e), e.Message);
+                return ValidationProblem();
+            }
+            EtudiantDto dto = new EtudiantDto().ToDto(etud);
+            // On revoie la route vers le get qu'on n'a pas encore écrit!
+            return CreatedAtAction(nameof(GetUnEtudiant), new { id = dto.Id }, dto);
         }
 
         // PUT api/<EtudiantController>/5
